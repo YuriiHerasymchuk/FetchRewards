409a0415dd86228d44ba5091bcf8627e36db9902
Yura <gerasumchyk@icloud.com>
2022-06-05 22:57:02 -0500

    Spend points logic

--- src/app.ts
+++ src/app.ts
@@ -13,11 +13,10 @@ class Transaction implements Transaction {
 }
 
 class PaymentService {
-    private transactions: Array<Transaction> = [];
-    private pointsLeftByTransactionId: Record<string, number> = {};
+    private transactions: Array<Transaction> = []; // all transactions
+    private pointsLeftByTransactionId: Record<string, number> = {}; // not allocated points by transaction ID
     private transactionsByUser: Record<string, Array<Transaction>> = {};
     private balanceByUser: Record<string, number> = {};
-
 
     constructor () {}
 
@@ -29,7 +28,7 @@ class PaymentService {
         const currentUserBalance = this.balanceByUser[userName] || 0;
         if (transactionPoints < 0) {
             if (currentUserBalance + transactionPoints < 0) throw new Error(`Not enough points to add transaction: ${JSON.stringify(transaction)}`);
-            this.subtractPointsForNegativeTransaction(usersTransactions, transaction)
+            this.subtractPointsFromTransactions(usersTransactions, -transaction.points, transaction.timestamp)
         } else {
             this.pointsLeftByTransactionId[transactionId] = transactionPoints;
         }
@@ -41,26 +40,42 @@ class PaymentService {
         PaymentService.sortTransactions(this.transactions);
         PaymentService.sortTransactions(usersTransactions);
     }
+    
+    spendPoints(pointsToSpend: number) {
+        this.subtractPointsFromTransactions(this.transactions, pointsToSpend);
+    }
 
-    subtractPointsForNegativeTransaction(transactions: Array<Transaction>, transaction: Transaction) {
-        let pointsToAllocate = -transaction.points;
-        for (const usersTransaction of transactions) {
-            if (usersTransaction.timestamp > transaction.timestamp) break;
-
-            const { _id: curTransactionId } = usersTransaction;
-            const points = this.pointsLeftByTransactionId[curTransactionId] || 0;
-            if (points > 0) {
-                if (points >= pointsToAllocate) {
-                    this.pointsLeftByTransactionId[curTransactionId] -= pointsToAllocate;
-                    pointsToAllocate = 0;
-                    break;
-                } else {
-                    pointsToAllocate -= this.pointsLeftByTransactionId[curTransactionId];
-                    delete this.pointsLeftByTransactionId[curTransactionId];
-                }
+    private subtractPointsFromTransactions(transactions: Array<Transaction>, pointsToAllocate: number, timestamp: Date | null = null) {
+        const transactionsForAllocation = this.filterTransactionsForAllocation(transactions, pointsToAllocate, timestamp);
+        
+        for (const transaction of transactionsForAllocation) {
+            const { _id: curTransactionId } = transaction;
+            const transactionPointsLeft = this.pointsLeftByTransactionId[curTransactionId] || 0;
+            if (transactionPointsLeft >= pointsToAllocate) {
+                this.pointsLeftByTransactionId[curTransactionId] -= pointsToAllocate;
+            } else {
+                pointsToAllocate -= this.pointsLeftByTransactionId[curTransactionId];
+                delete this.pointsLeftByTransactionId[curTransactionId];
             }
         }
-        if (pointsToAllocate > 0) throw new Error(`Not enough points at the time of the transaction: ${JSON.stringify(transaction)}`);
+    }
+
+    private filterTransactionsForAllocation(transactions: Array<Transaction>, pointsToAllocate: number, timestamp: Date | null = null): Array<Transaction> {
+        const transactionsForAllocation: Array<Transaction> = [];
+
+        for (const transaction of transactions) {
+            if (timestamp && transaction.timestamp > timestamp) break;
+
+            const transactionPointsLeft = this.pointsLeftByTransactionId[transaction._id] || 0;
+            if (transactionPointsLeft > 0) {
+                pointsToAllocate -= this.pointsLeftByTransactionId[transaction._id];
+                transactionsForAllocation.push(transaction);
+            }
+            if (pointsToAllocate <= 0) break;
+        }
+        if (pointsToAllocate > 0) throw new Error(`Not enough points`);
+
+        return transactionsForAllocation;
     }
 
     printTransactions() {
@@ -81,7 +96,9 @@ try {
     ps.addTransaction(new Transaction('UNILEVER', 200, new Date('2020-10-31T11:00:00Z')));
     ps.addTransaction(new Transaction('DANNON', 300, new Date('2020-10-31T10:00:00Z')));
     ps.addTransaction(new Transaction('DANNON', -200, new Date('2020-10-31T15:00:00Z')));
+    ps.addTransaction(new Transaction('DANNON', -200, new Date('2020-11-03T14:00:00Z')));
     ps.addTransaction(new Transaction('MILLER COORS', 10000, new Date('2020-11-01T14:00:00Z')));
+    ps.spendPoints(5000);
 } catch (err) {
     console.error(err);
 }
